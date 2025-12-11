ddc57de495d7502ee17a443038aba0f33dbab80d
Infinity0075 <ap990075@gmail.com>
2025-12-11 16:05:56 +0530

    changes

--- frontend/src/pages/Notifications.jsx
+++ frontend/src/pages/Notifications.jsx
@@ -1,6 +1,23 @@
 import React from 'react'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { acceptFriendRequest, getFriendRequests } from '../lib/api'
 
 const Notification = () => {
+  const queryClient = useQueryClient()
+
+  const { data: friendRequests, isLoading } = useQuery({
+    queryKey: ['friendRequests'],
+    queryFn: getFriendRequests
+  })
+
+  const { mutate: acceptRequestMutation, isPending } = useMutation({
+    mutationFn: acceptFriendRequest,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['friendRequests'] })
+      queryClient.invalidateQueries({ queryKey: ['friends'] })
+    }
+  })
+
   return (
     <div>
       <h1>Page...</h1>
