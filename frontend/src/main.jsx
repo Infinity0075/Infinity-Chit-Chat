90ccf48a71c29e0e1e23ed471460b5dccddbc65a
Infinity0075 <ap990075@gmail.com>
2025-12-12 00:34:34 +0530

    addded chat and video call section

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import 'stream-chat-react/dist/css/v2/index.css'
+import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
   </BrowserRouter>
-);
+)
